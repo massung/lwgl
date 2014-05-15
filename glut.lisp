5fa2e3ea01f6cf59f7cb42807952e3aa6e99dd3c
Jeffrey Massung <massung@gmail.com>
2014-05-15 15:51:10 -0500

    Changed glut-pane callbacks and updated the sample to blend.

--- glut.lisp
+++ glut.lisp
@@ -23,22 +23,57 @@
    #:glut-pane
    #:glut-pane-timer-delay
    #:glut-pane-timer-callback
-   #:glut-pane-keyboard-callback
-   #:glut-pane-mouse-callback
-   #:glut-pane-motion-callback))
+   #:glut-pane-key-press-callback
+   #:glut-pane-key-release-callback
+   #:glut-pane-mouse-press-callback
+   #:glut-pane-mouse-release-callback
+   #:glut-pane-motion-callback
+   #:glut-pane-passive-motion-callback))
 
 (in-package :opengl-glut-pane)
 
 (defclass glut-pane (opengl-pane)
-  ((keyboard-callback :initform nil :initarg :keyboard-callback :accessor glut-pane-keyboard-callback)
-   (mouse-callback    :initform nil :initarg :mouse-callback    :accessor glut-pane-mouse-callback)
-   (motion-callback   :initform nil :initarg :motion-callback   :accessor glut-pane-motion-callback)
-   (timer-callback    :initform nil :initarg :timer-callback    :accessor glut-pane-timer-callback)
-   (timer-delay       :initform nil :initarg :timer-delay       :reader   glut-pane-timer-delay)
-   (timer-last        :initform nil)
-   (timer             :initform nil))
+  ((timer
+    :initform nil)
+   (timer-last
+    :initform nil)
+   (timer-delay
+    :initform nil
+    :initarg :timer-delay
+    :reader glut-pane-timer-delay)
+   (timer-callback
+    :initform nil
+    :initarg :timer-callback
+    :accessor glut-pane-timer-callback)
+   (key-press-callback
+    :initform nil
+    :initarg :key-press-callback
+    :accessor glut-pane-key-press-callback)
+   (key-release-callback
+    :initform nil
+    :initarg :key-release-callback
+    :accessor glut-pane-key-release-callback)
+   (mouse-press-callback
+    :initform nil
+    :initarg :mouse-press-callback
+    :accessor glut-pane-mouse-press-callback)
+   (mouse-release-callback
+    :initform nil
+    :initarg :mouse-release-callback
+    :accessor glut-pane-mouse-release-callback)
+   (motion-callback
+    :initform nil
+    :initarg :motion-callback
+    :accessor glut-pane-motion-callback)
+   (passive-motion-callback
+    :initform nil
+    :initarg :passive-motion-callback
+    :accessor glut-pane-passive-motion-callback))
   (:default-initargs
-   :input-model '(((:motion)            glut-pane-motion)
+   :input-model '(((:motion)            glut-pane-passive-motion)
+                  ((:button-1 :motion)  glut-pane-motion)
+                  ((:button-2 :motion)  glut-pane-motion)
+                  ((:button-3 :motion)  glut-pane-motion)
                   ((:button-1 :press)   glut-pane-button-1-press)
                   ((:button-2 :press)   glut-pane-button-2-press)
                   ((:button-3 :press)   glut-pane-button-3-press)
@@ -48,7 +83,7 @@
                   ((:key :press)        glut-pane-key-press)
                   ((:key :release)      glut-pane-key-release))))
 
-(defmethod create-opengl-pane progn ((pane glut-pane))
+(defmethod opengl-pane:create-opengl-pane progn ((pane glut-pane))
   "Create the timer if there is one to be created."
   (with-slots (timer timer-delay timer-last)
       pane
@@ -59,7 +94,7 @@
     (when timer-delay
       (schedule-timer-relative timer timer-delay timer-delay))))
 
-(defmethod destroy-opengl-pane progn ((pane glut-pane))
+(defmethod opengl-pane:destroy-opengl-pane progn ((pane glut-pane))
   "Stop the periodic timer."
   (unschedule-timer (slot-value pane 'timer)))
 
@@ -84,6 +119,11 @@
         (funcall timer-callback pane (/ (- now timer-last) internal-time-units-per-second)))
       (setf timer-last now))))
 
+(defmethod glut-pane-passive-motion ((pane glut-pane) x y)
+  "Called whenever the mouse moves."
+  (when-let (motion-callback (glut-pane-passive-motion-callback pane))
+    (funcall motion-callback pane x y)))
+
 (defmethod glut-pane-motion ((pane glut-pane) x y)
   "Called whenever the mouse moves."
   (when-let (motion-callback (glut-pane-motion-callback pane))
@@ -91,42 +131,42 @@
 
 (defmethod glut-pane-button-1-press ((pane glut-pane) x y)
   "Called whenever the left mouse button is pressed."
-  (when-let (mouse-callback (glut-pane-mouse-callback pane))
-    (funcall mouse-callback pane :left t x y)))
+  (when-let (mouse-callback (glut-pane-mouse-press-callback pane))
+    (funcall mouse-callback pane :button-1 x y)))
 
 (defmethod glut-pane-button-2-press ((pane glut-pane) x y)
   "Called whenever the left mouse button is pressed."
-  (when-let (mouse-callback (glut-pane-mouse-callback pane))
-    (funcall mouse-callback pane :middle t x y)))
+  (when-let (mouse-callback (glut-pane-mouse-press-callback pane))
+    (funcall mouse-callback pane :button-2 x y)))
 
 (defmethod glut-pane-button-3-press ((pane glut-pane) x y)
   "Called whenever the left mouse button is pressed."
-  (when-let (mouse-callback (glut-pane-mouse-callback pane))
-    (funcall mouse-callback pane :right t x y)))
+  (when-let (mouse-callback (glut-pane-mouse-press-callback pane))
+    (funcall mouse-callback pane :button-3 x y)))
 
 (defmethod glut-pane-button-1-release ((pane glut-pane) x y)
   "Called whenever the left mouse button is released."
-  (when-let (mouse-callback (glut-pane-mouse-callback pane))
-    (funcall mouse-callback pane :left nil x y)))
+  (when-let (mouse-callback (glut-pane-mouse-release-callback pane))
+    (funcall mouse-callback pane :button-1 x y)))
 
 (defmethod glut-pane-button-2-release ((pane glut-pane) x y)
   "Called whenever the left mouse button is released."
-  (when-let (mouse-callback (glut-pane-mouse-callback pane))
-    (funcall mouse-callback pane :middle nil x y)))
+  (when-let (mouse-callback (glut-pane-mouse-release-callback pane))
+    (funcall mouse-callback pane :button-2 x y)))
 
 (defmethod glut-pane-button-3-release ((pane glut-pane) x y)
   "Called whenever the left mouse button is released."
-  (when-let (mouse-callback (glut-pane-mouse-callback pane))
-    (funcall mouse-callback pane :right nil x y)))
+  (when-let (mouse-callback (glut-pane-mouse-release-callback pane))
+    (funcall mouse-callback pane :button-3 x y)))
 
 (defmethod glut-pane-key-press ((pane glut-pane) x y gspec)
   "Called whenevera key is pressed."
-  (when-let (keyboard-callback (glut-pane-keyboard-callback pane))
-    (funcall keyboard-callback pane gspec t x y)))
+  (when-let (keyboard-callback (glut-pane-key-press-callback pane))
+    (funcall keyboard-callback pane x y gspec)))
 
 (defmethod glut-pane-key-release ((pane glut-pane) x y gspec)
   "Called whenever a key is released."
-  (when-let (keyboard-callback (glut-pane-keyboard-callback pane))
-    (funcall keyboard-callback pane gspec nil x y)))
+  (when-let (keyboard-callback (glut-pane-key-release-callback pane))
+    (funcall keyboard-callback pane x y gspec)))
 
 
